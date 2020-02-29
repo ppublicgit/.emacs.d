dc1ffbbd8c319a87712a198573ae12bd720b9aa4
ppublicgit <p.public@protonmail.com>
2020-02-28 21:15:26 -0600

    Add all downloaded packages
    Signed-off-by: ppublicgit <p.public@protonmail.com>

---
+++ elpa/flycheck-rtags-20191222.920/flycheck-rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-rtags" "20191222.920" "RTags Flycheck integration" '((emacs "24") (flycheck "0.23") (rtags "2.10")) :commit "6d6ea68919ae80585b8396cd642c157e7a38887d" :authors '(("Christian Schwarzgruber" . "c.schwarzgruber.cs@gmail.com")) :maintainer '("Christian Schwarzgruber" . "c.schwarzgruber.cs@gmail.com") :url "https://github.com/Andersbakken/rtags")
