41e5fbb2d34aefeed60ef3be21e37801f2802371
P Public <p.public@protonmail.com>
2020-07-25 09:39:53 -0500

    removed elpa

--- elpa/dracula-theme-20200122.1904/dracula-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dracula-theme" "20200122.1904" "Dracula Theme" '((emacs "24")) :commit "2537e8e5b122769b1ac0fd87e0f430e90c5209e2" :authors '(("film42")) :maintainer '("film42") :url "https://github.com/dracula/emacs")
--- elpa/helm-rtags-20191222.920/helm-rtags-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "helm-rtags" "20191222.920" "A front-end for rtags" '((helm "2.0") (rtags "2.10")) :commit "5f4b467e5a57edd8084fa49ce130af12f78f2ea6" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "https://github.com/Andersbakken/rtags")
