9c9cbaf49d6100e38b6241020778e7be318da7a9
P Public <p.public@protonmail.com>
2020-07-25 09:44:14 -0500

    removed elpa success

--- init.el
+++ init.el
@@ -4,7 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elpy-rpc-pythonpath "/home/p/.emacs.d/elpa/elpy-20200329.1830")
  '(package-selected-packages
    (quote
     (markdown-preview-mode sr-speedbar web-mode irony helm-rtags company-rtags rtags ace-jump-mode use-package haskell-mode dracula-theme))))
